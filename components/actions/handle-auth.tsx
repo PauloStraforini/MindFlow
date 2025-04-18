88f03fdc2218fb0ff93fc679e709d14353b83c6c
PauloStraforini <paulostraforini@gmail.com>
2025-04-18 11:08:06 -0300

    ✨ Add handleAuth and handleSingOut functions for authentication flow

---
+++ components/actions/handle-auth.tsx
@@ -0,0 +1,10 @@
+"use server";
+import { signIn } from "@/lib/auth";
+
+
+export async function handleAuth() {
+    
+  await signIn("google, linkedin", {
+    redirectTo: "/psicologos/dashboard",
+  });
+}

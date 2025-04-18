82c03b2f466c686665584b6ebb4599fd51308ae6
PauloStraforini <paulostraforini@gmail.com>
2025-04-18 11:07:32 -0300

    ✨ Add LinkedIn provider to NextAuth configuration

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,9 +1,10 @@
 
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
+import LinkedIn from "next-auth/providers/linkedin"
 
 
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
+  providers: [Google, LinkedIn],
 })

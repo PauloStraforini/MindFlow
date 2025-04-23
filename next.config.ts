911727d899941eb34a4175d65267b3192b3edb31
PauloStraforini <paulostraforini@gmail.com>
2025-04-23 17:23:16 -0300

    Remove eslint and eslint-config-next dependencies from package.json

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true, // 💥 Desativa o ESLint durante o build
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
